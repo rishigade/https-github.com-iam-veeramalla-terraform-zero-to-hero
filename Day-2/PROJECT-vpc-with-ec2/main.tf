545f811e9ca7d5a3b655f6f596522acf765363ea
RishiGade <gaderishikesh8@gmail.com>
2025-04-06 22:26:30 +0530

    updated the values

--- Day-2/PROJECT-vpc-with-ec2/main.tf
+++ Day-2/PROJECT-vpc-with-ec2/main.tf
@@ -1,27 +1,27 @@
-resource "aws_vpc" "myvpc" {
+resource "aws_vpc" "Rishi_eTop" {
   cidr_block = var.cidr
 }
 
 resource "aws_subnet" "sub1" {
-  vpc_id                  = aws_vpc.myvpc.id
+  vpc_id                  = aws_vpc.Rishi_eTop.id
   cidr_block              = "10.0.0.0/24"
-  availability_zone       = "us-east-1a"
+  availability_zone       = "eu-west-1a"
   map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "sub2" {
-  vpc_id                  = aws_vpc.myvpc.id
+  vpc_id                  = aws_vpc.Rishi_eTop.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1b"
+  availability_zone       = "eu-west-1b"
   map_public_ip_on_launch = true
 }
 
 resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.myvpc.id
+  vpc_id = aws_vpc.Rishi_eTop.id
 }
 
 resource "aws_route_table" "RT" {
-  vpc_id = aws_vpc.myvpc.id
+  vpc_id = aws_vpc.Rishi_eTop.id
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -41,7 +41,7 @@ resource "aws_route_table_association" "rta2" {
 
 resource "aws_security_group" "webSg" {
   name   = "web"
-  vpc_id = aws_vpc.myvpc.id
+  vpc_id = aws_vpc.Rishi_eTop.id
 
   ingress {
     description = "HTTP from VPC"
@@ -92,8 +92,8 @@ resource "aws_instance" "webserver2" {
 }
 
 #create alb
-resource "aws_lb" "myalb" {
-  name               = "myalb"
+resource "aws_lb" "eTop_alb" {
+  name               = "eTop_alb"
   internal           = false
   load_balancer_type = "application"
 
@@ -105,11 +105,11 @@ resource "aws_lb" "myalb" {
   }
 }
 
-resource "aws_lb_target_group" "tg" {
+resource "aws_lb_target_group" "eTop_tg" {
   name     = "myTG"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = aws_vpc.myvpc.id
+  vpc_id   = aws_vpc.Rishi_eTop.id
 
   health_check {
     path = "/"
@@ -118,28 +118,28 @@ resource "aws_lb_target_group" "tg" {
 }
 
 resource "aws_lb_target_group_attachment" "attach1" {
-  target_group_arn = aws_lb_target_group.tg.arn
+  target_group_arn = aws_lb_target_group.eTop_tg.arn
   target_id        = aws_instance.webserver1.id
   port             = 80
 }
 
 resource "aws_lb_target_group_attachment" "attach2" {
-  target_group_arn = aws_lb_target_group.tg.arn
+  target_group_arn = aws_lb_target_group.eTop_tg.arn
   target_id        = aws_instance.webserver2.id
   port             = 80
 }
 
 resource "aws_lb_listener" "listener" {
-  load_balancer_arn = aws_lb.myalb.arn
+  load_balancer_arn = aws_lb.eTop_alb.arn
   port              = 80
   protocol          = "HTTP"
 
   default_action {
-    target_group_arn = aws_lb_target_group.tg.arn
+    target_group_arn = aws_lb_target_group.eTop_tg.arn
     type             = "forward"
   }
 }
 
 output "loadbalancerdns" {
-  value = aws_lb.myalb.dns_name
+  value = aws_lb.eTop_alb.dns_name
 }
