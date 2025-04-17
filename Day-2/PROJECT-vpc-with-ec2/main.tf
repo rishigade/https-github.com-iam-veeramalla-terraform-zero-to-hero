0c824ab12f4b5367aa0b8bfb4522e3a727ff3367
RishiGade <gaderishikesh8@gmail.com>
2025-04-17 19:27:51 +0530

    updated files

--- Day-2/PROJECT-vpc-with-ec2/main.tf
+++ Day-2/PROJECT-vpc-with-ec2/main.tf
@@ -77,7 +77,7 @@ resource "aws_s3_bucket" "example" {
 
 resource "aws_instance" "webserver1" {
   ami                    = "ami-0261755bbcb8c4a84"
-  instance_type          = "t2.micro"
+  instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.webSg.id]
   subnet_id              = aws_subnet.sub1.id
   user_data              = base64encode(file("userdata.sh"))
@@ -85,7 +85,7 @@ resource "aws_instance" "webserver1" {
 
 resource "aws_instance" "webserver2" {
   ami                    = "ami-0261755bbcb8c4a84"
-  instance_type          = "t2.micro"
+  instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.webSg.id]
   subnet_id              = aws_subnet.sub2.id
   user_data              = base64encode(file("userdata1.sh"))
--- Day-2/PROJECT-vpc-with-ec2/variables.tf
+++ Day-2/PROJECT-vpc-with-ec2/variables.tf
@@ -1,3 +1,10 @@
 variable "cidr" {
   default = "10.0.0.0/16"
 }
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
