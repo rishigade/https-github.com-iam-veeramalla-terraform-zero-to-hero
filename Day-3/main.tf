243debf0bb369fe67e8c55f58c67759a9528ef6d
RishiGade <gaderishikesh8@gmail.com>
2025-04-18 10:12:00 +0530

    update the files

--- Day-3/main.tf
+++ Day-3/main.tf
@@ -4,4 +4,8 @@ provider "aws" {
 
 module "ec2_instance" {
   source = "./modules/ec2_instance"
+  ami_value  = "ami-01621ce8f257d0d13"
+  instance_type_value = "t2.micro"
+  subnet_id_value  = "subnet-0a9c401a2324004a8"
+  environment = "DEV"
 }
